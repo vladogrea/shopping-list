bc3214d3f85938f1b182a3ebd835b0b96ee37b60
Gorea Vlad <goreavladoctav@gmail.com>
2024-03-12 21:05:08 +0200

    API endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+
+Route::prefix('v1')->group(function() {
+    Route::apiResource('/tasks', TaskController::class);
+});
+
+
+
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
