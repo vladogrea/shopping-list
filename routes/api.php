ea431acf336a4d717b4701293f2f4bf5c7b8711f
Gorea Vlad <goreavladoctav@gmail.com>
2024-03-13 20:00:25 +0200

    update data

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CompleteTaskController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function() {
     Route::apiResource('/tasks', TaskController::class);
+    Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
 
 
